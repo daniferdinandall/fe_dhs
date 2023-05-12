0dba48d83b960ccdc41e06b480bb16dee47c931e
daniferdinandall <daniferdinandall@gmail.com>
2023-05-12 15:16:43 +0700

    udpate

--- js/config/url.js
+++ js/config/url.js
@@ -1,9 +1,2 @@
 export let urlAPI = "https://ws-dani.herokuapp.com/dhs-all";
 export let urlGetPresensi = "https://ws-dani.herokuapp.com/dhs-all";
-export let urlPOST = "https://ws-dani.herokuapp.com/ins"
-
-export function AmbilResponse(result) {
-    console.log(result); //menampilkan response API pada console
-    alert(result.message); //menampilkan response API pada alert
-    window.location.reload(); //reload halaman setelah klik ok pada alert
-}
