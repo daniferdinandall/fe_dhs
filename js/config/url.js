216a0bea00ae5c7bf7635aa1bbd0dab73b0e3ab2
daniferdinandall <daniferdinandall@gmail.com>
2023-05-12 15:44:35 +0700

    update

--- js/config/url.js
+++ js/config/url.js
@@ -1,2 +1,2 @@
 export let urlAPI = "https://ws-dani.herokuapp.com/dhs-all";
-export let urlGetPresensi = "https://ws-dani.herokuapp.com/dhs-all";
+export let urlGetPresensi = "https://ws-dani.herokuapp.com/presensi";
