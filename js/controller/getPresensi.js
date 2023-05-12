43c204748b07a292baedf82f5f9204e71fe96023
daniferdinandall <daniferdinandall@gmail.com>
2023-05-12 16:56:02 +0700

    udpate

--- js/controller/getPresensi.js
+++ js/controller/getPresensi.js
@@ -2,12 +2,17 @@ import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabelPresensi } from "../temp/table.js";
 export function isiTablePresensi(results) {
-    results.reverse().forEach(isiRow);
+    // results.reverse().forEach(isiRow);
     // let value2 = value.sort((a, b) => (new Date(b.datetime)).getTime() - (new Date(a.datetime)).getTime());
+    Myvar.length = results.length;
+    results.sort((a, b) => new Date(b.datetime) - new Date(a.datetime)); //mengurutkan berdasasrkan datetime in descending order
+    results.forEach(isiRow);
+  
 }
 function isiRow(value) {
     // (new Date("2009-05-12T08:25:08.551Z")).getTime()
     // console.log(value)
+    document.getElementById("jml").innerHTML = "" + Myvar.length + " Data";
     let content =
         isiTabelPresensi.replace("#NAMA#", value.biodata.nama?value.biodata.nama:"#NAMA#")
             .replace("#NOHP#", value.biodata.phone_number ? value.biodata.phone_number : (value.phone_number ? value.phone_number : '#NOHP#'))
