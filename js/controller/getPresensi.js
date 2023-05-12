9a2ea1b5678548f1ce1844740edd487cd29b13a6
daniferdinandall <daniferdinandall@gmail.com>
2023-05-12 16:23:48 +0700

    udpate

--- js/controller/getPresensi.js
+++ js/controller/getPresensi.js
@@ -2,22 +2,22 @@ import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabelPresensi } from "../temp/table.js";
 export function isiTablePresensi(results) {
-    results.forEach(isiRow);
+    results.reverse().forEach(isiRow);
+    // let value2 = value.sort((a, b) => (new Date(b.datetime)).getTime() - (new Date(a.datetime)).getTime());
 }
 function isiRow(value) {
     // (new Date("2009-05-12T08:25:08.551Z")).getTime()
-    let value2 = value.sort((a, b) => (new Date(b.datetime)).getTime() - (new Date(a.datetime)).getTime());
-    console.log(value2)
+    // console.log(value)
     let content =
-        isiTabelPresensi.replace("#NAMA#", value2.biodata.nama?value2.biodata.nama:"#NAMA#")
-            .replace("#NOHP#", value2.biodata.phone_number ? value2.biodata.phone_number : (value2.phone_number ? value2.phone_number : '#NOHP#'))
-            .replace("#JABATAN#", value2.biodata.jabatan)
-            .replace("#LOKASI#", value2.location)
-            .replace("#STATUS#", value2.checkin)
-            .replace("#HARIKERJA#", value2.biodata.hari_kerja ? value2.biodata.hari_kerja : "HARIKERJA")
-            .replace("#JAMKERJA#", value2.biodata.jam_kerja ? value2.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
-            .replace("#JAMMASUK#", value2.biodata.jam_kerja ? value2.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
-            .replace("#JAMKELUAR#", value2.biodata.jam_kerja ? value2.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
+        isiTabelPresensi.replace("#NAMA#", value.biodata.nama?value.biodata.nama:"#NAMA#")
+            .replace("#NOHP#", value.biodata.phone_number ? value.biodata.phone_number : (value.phone_number ? value.phone_number : '#NOHP#'))
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja ? value.biodata.hari_kerja : "HARIKERJA")
+            .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
+            .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
+            .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
     addInner("iniTabelPresensi", content);
