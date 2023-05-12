68cad5b8e3aaa0474c4b9acbac31bedc6304d12e
daniferdinandall <daniferdinandall@gmail.com>
2023-05-12 15:35:07 +0700

    update

---
+++ js/controller/getPresensi.js
@@ -0,0 +1,22 @@
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabelPresensil } from "../temp/table.js";
+export function isiTablePresensi(results) {
+    results.forEach(isiRow);
+}
+function isiRow(value) {
+    // console.log(value.jam_kerja)
+    let content =
+        isiTabelPresensil.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number ? value.biodata.phone_number : (value.phone_number ? value.phone_number : '#NOHP#'))
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja ? value.biodata.hari_kerja : "HARIKERJA")
+            .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
+            .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
+            .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+    addInner("iniTabel", content);
+}
